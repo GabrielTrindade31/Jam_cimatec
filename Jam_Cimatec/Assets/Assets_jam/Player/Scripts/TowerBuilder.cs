e22b48f4fceb819e59cd318afe74b141ba070930
Gabriel-Petersen <gabrielf.petersen@gmail.com>
2025-05-24 00:47:30 -0300

    Instanciação de construções

---
+++ Jam_Cimatec/Assets/Assets_jam/Player/Scripts/TowerBuilder.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(PlayerInput), typeof(PlayerController))]
+public class TowerBuilder : MonoBehaviour
+{
+    public Tilemap tilemap;
+    [SerializeField] private List<BuildEntry> buildSet = new();
+    public BuildType selectedBuildType;
+    public int buildIndex;
+    private Dictionary<BuildType, List<Build>> enabledBuilds;
+    private Dictionary<Vector2Int, GameObject> notEmptySpaces = new();
+    private PlayerInput input;
+    private PlayerController playerController;
+    private GameObject currentGhost;
+    void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        playerController = GetComponent<PlayerController>();
+        SetupBuilds();
+    }
+
+    void SetupBuilds()
+    {
+        enabledBuilds = new Dictionary<BuildType, List<Build>>();
+
+        foreach (var entry in buildSet)
+        {
+            if (!enabledBuilds.ContainsKey(entry.buildType))
+                enabledBuilds[entry.buildType] = new List<Build>();
+
+            enabledBuilds[entry.buildType].AddRange(entry.constructions);
+        }
+    }
+
+    void OnEnable()
+    {
+        input.actions["Previous"].performed += _ => PreviousBuilding();
+        input.actions["Next"].performed += _ => NextBuilding();
+        input.actions["Jump"].performed += _ => ChangeBuildMode();
+        input.actions["Fire"].performed += _ => TryBuild();
+    }
+
+    void OnDisable()
+    {
+        input.actions["Previous"].performed -= _ => PreviousBuilding();
+        input.actions["Next"].performed -= _ => NextBuilding();
+        input.actions["Jump"].performed -= _ => ChangeBuildMode();
+        input.actions["Fire"].performed -= _ => TryBuild();
+    }
+
+    void Update()
+    {
+        if (playerController.isBuilding)
+            UpdateGhostPosition();
+        else
+            currentGhost = null;   
+    }
+
+    void ChangeBuildMode()
+    {
+        playerController.isBuilding = !playerController.isBuilding;
+    }
+
+    void PreviousBuilding()
+    {
+        SelectBuild(buildIndex - 1);
+    }
+
+    void NextBuilding()
+    {
+        SelectBuild(buildIndex + 1);
+    }
+
+    void SelectBuild(int index)
+    {
+        if (index >= 0 && index < enabledBuilds[selectedBuildType].Count)
+        {
+            buildIndex = index;
+
+            if (currentGhost != null)
+                Destroy(currentGhost);
+
+            if (playerController.isBuilding)
+                currentGhost = Instantiate(enabledBuilds[selectedBuildType][buildIndex].CreateGhost());
+        }
+    }
+
+    void UpdateGhostPosition()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        mousePos.z = 0;
+        Vector2Int gridPos = World2Grid(mousePos);
+        Vector3 ghostPos = Grid2World(gridPos);
+        if (playerController.isBuilding && currentGhost == null)
+            SelectBuild(buildIndex);
+
+        currentGhost.transform.position = ghostPos;
+    }
+
+    Vector2Int World2Grid(Vector3 worldPos)
+    {
+        Vector3Int cellPos = tilemap.WorldToCell(worldPos);
+        return new Vector2Int(cellPos.x, cellPos.y);
+    }
+
+    Vector3 Grid2World(Vector2Int gridPos)
+    {
+        return tilemap.GetCellCenterWorld(new Vector3Int(gridPos.x, gridPos.y, 0));
+    }
+
+    void TryBuild()
+    {
+        if (playerController.isBuilding)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            mousePos.z = 0;
+            Vector2Int gridPos = World2Grid(mousePos);
+
+            if (notEmptySpaces.ContainsKey(gridPos)) return;
+
+            Vector3 finalPosition = Grid2World(gridPos);
+            GameObject newTower = Instantiate(enabledBuilds[selectedBuildType][buildIndex].prefab, finalPosition, Quaternion.identity);
+            notEmptySpaces[gridPos] = newTower;
+        }
+    }
+}
